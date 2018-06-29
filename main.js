abdb9016816040a6bda969044b622922d49c0438
retrodaredevil <retrodaredevil@gmail.com>
2018-06-28 21:39:55 -0500

    Commit before using RobotRanking class in team/index.js

--- main.js
+++ main.js
@@ -179,3 +179,255 @@ function getAuthKey(){
 function setAuthKey(authKey){
 	setQueryKey("auth", authKey);
 }
+
+function getDesiredYear(){
+	let year = getQueryObject()["year"];
+	if(year === undefined || year === "null"){
+		let r = new Date().getFullYear();
+		setDesiredYear(r);
+		return r;
+	}
+
+	return year;
+}
+function setDesiredYear(year){
+	if(year === null || year === undefined) throw "Year cannot be null or undefined";
+
+	setQueryKey("year", year);
+	for(let yearElement of document.getElementsByClassName("current_year")){
+		yearElement.innerText = year;
+	}
+}
+
+function getEventDate(event){
+	let dateString = event.start_date;
+	let split = dateString.split("-");
+	return new Date(+split[0], +split[1], +split[2]);
+}
+
+
+class RobotRanking extends Object{
+	constructor(teamNumber, matches){
+		super();
+		this.teamNumber = teamNumber;
+		this.teamKey = "frc" + teamNumber;
+
+		this.eventsAttendedKeys = [];
+		this.countableMatches = 0; // aka breakdownable matches. Should be used with more advanced stats
+
+		this.totalMatches = matches.length; // should be used with wins losses, etc.
+
+		this.totalWins = 0;
+		this.totalLosses = 0;
+		this.totalTies = 0;
+
+		this.qualWins = 0;
+		this.qualLosses = 0;
+		this.qualTies = 0;
+		this.playoffWins = 0;
+		this.playoffLosses = 0;
+		this.playoffTies = 0;
+
+		this.autoPointsTotal = 0;
+		this.telopPointsTotal = 0;
+		this.totalPointsTotal = 0;
+		this.foulPointsReceivedTotal = 0;
+
+		this.teamFoulCommittedTotal = 0;
+		this.teamTechFoulCommittedTotal = 0;
+
+
+		this.successAutoRunCount = 0;
+		this.unknownAutoRunCount = 0; // Amount of auto runs we don't have data on. Usually == totalMatches - countableMatches but use this for more accuracy
+
+		for(let match of matches){
+			let matchEventKey = match.event_key;
+			if(!this.eventsAttendedKeys.includes(matchEventKey)){
+				this.eventsAttendedKeys.push(matchEventKey);
+			}
+			let isOnBlue = this.isRobotBlue(match);
+
+			let teamBreakdown = null;
+			if(match.score_breakdown !== null){
+				this.countableMatches++;
+				teamBreakdown = (isOnBlue ? match.score_breakdown.blue : match.score_breakdown.red);
+			}
+			let isPlayoff = match.comp_level !== "qm";
+
+			// === Wins and Losses
+			if((match.winning_alliance === "blue" && isOnBlue) || (match.winning_alliance === "red" && !isOnBlue)){
+				// we won
+				this.totalWins++;
+				if(isPlayoff){
+					this.playoffWins++;
+				} else {
+					this.qualWins++;
+				}
+			} else if(match.winning_alliance === "red" || match.winning_alliance === "blue"){ // check to make sure winning_alliance is valid
+				// we lost
+				this.totalLosses++;
+				if(isPlayoff){
+					this.playoffLosses++;
+				} else{
+					this.qualLosses++;
+				}
+			} else {
+				// possibly a tie
+				if(match.score_breakdown === null){
+					//tie
+					this.totalTies++;
+					if(isPlayoff){
+						this.playoffTies++;
+					} else {
+						this.qualTies++;
+					}
+				} else {
+					console.log("winning_alliance is: '" + match.winning_alliance + "'. It is either a tie or we will use the score to figure out who won.");
+					let bluePoints = match.score_breakdown.blue.total_points;
+					let redPoints = match.score_breakdown.red.total_points;
+					if (bluePoints === redPoints) {
+						//tie
+						this.totalTies++;
+						if(isPlayoff){
+							this.playoffTies++;
+						} else {
+							this.qualTies++;
+						}
+					} else if ((bluePoints > redPoints && isOnBlue) || (redPoints > bluePoints && !isOnBlue)) {
+						// win
+						this.totalWins++;
+						if(isPlayoff){
+							this.playoffWins++;
+						} else{
+							this.qualWins++;
+						}
+					} else {
+						// loss
+						this.totalLosses++;
+						if(isPlayoff){
+							this.playoffLosses++;
+						} else{
+							this.qualLosses++;
+						}
+					}
+				}
+			}
+			// === End Wins and Losses ===
+			let robotAuto;
+			if(match.score_breakdown === null){ // 2014 and before is always like this
+				robotAuto = "Unknown";
+			} else {
+				robotAuto = teamBreakdown["autoRobot" + this.getRobotNumber(match)];
+			}
+			if(robotAuto === "Unknown"){
+				this.unknownAutoRunCount++;
+			} else if(robotAuto !== "None"){
+				this.successAutoRunCount++;
+			}
+			if(match.score_breakdown !== null){
+				this.autoPointsTotal += match.score_breakdown.autoPoints;
+				this.telopPointsTotal += match.score_breakdown.teleopPoints;
+				this.totalPointsTotal += match.score_breakdown.totalPoints;
+				this.foulPointsReceivedTotal += match.score_breakdown.foulPoints;
+
+				this.teamTechFoulCommittedTotal += match.score_breakdown.techFoulCount;
+				this.teamFoulCommittedTotal += match.score_breakdown.foulCount;
+			}
+		}
+	}
+	isRobotBlue(match){
+		return match.alliances.blue.team_keys.includes(this.teamKey);
+	}
+	getRobotNumber(match){
+		let isOnBlue = this.isRobotBlue(match);
+		return 1 + (isOnBlue ? match.alliances.blue.team_keys : match.alliances.red.team_keys).indexOf(this.teamKey);
+	}
+}
+class RobotRanking2018 extends RobotRanking{
+	constructor(teamNumber, matches){
+		super(teamNumber, matches);
+		/** The number of matches that affected these rankings (if a match had no score_breakdown, it wouldn't count as a "countable match" */
+		this.rankingPointsTotal = 0;
+
+		this.endgameNothingTotal = 0; // "None"
+		this.endgameLevitateTotal = 0; // "Levitate"
+		this.endgameParkingTotal = 0; // "Parking"
+		this.endgameClimbTotal = 0; // "Climbing"
+
+		this.numberDoubleClimbs = 0;
+		this.numberTripleClimbs = 0;
+
+		this.teamAutoSwitchOwnershipTotalSeconds = 0;
+		this.timesTeamOwnSwitchAfterAuto = 0; // only incremented by 1 each time above is incremented
+		this.teamAutoScaleOwnershipTotalSeconds = 0;
+		this.timesTeamOwnScaleAfterAuto = 0;
+
+		this.teamTeleopSwitchOwnershipTotalSeconds = 0;
+		this.teamTeleopScaleOwnershipTotalSeconds = 0;
+
+		this.timesBoostPlayed = 0; // number of times boost was played
+		// this.powerupBoostPowerCubeTotal = 0; // total amount of power cubes that were played for boost powerup
+		this.timesForcePlayed = 0;
+		// this.powerupForcePowerCubeTotal = 0;
+		this.timesLevitatePlayed = 0;
+
+		this.totalCubesAtMatchEndTotal = 0; // total number of cubes in the vault
+
+		for(let match of matches){
+			if(match.score_breakdown === null){
+				continue;
+			}
+			let isOnBlue = this.isRobotBlue(match);
+			let breakdown = (isOnBlue ? match.score_breakdown.blue : match.score_breakdown.red);
+
+			this.rankingPointsTotal += breakdown.rp;
+			let endgame = breakdown["endgameRobot" + this.getRobotNumber(match)];
+			switch(endgame){
+				case "Climbing":
+					this.endgameClimbTotal++;
+					break;
+				case "Parking":
+					this.endgameParkingTotal++;
+					break;
+				case "Levitate":
+					this.endgameLevitateTotal++;
+					break;
+				case "None":
+					this.endgameNothingTotal++;
+					break;
+				default:
+					console.log("got: '" + endgame + "' as endgame.");
+					this.endgameNothingTotal++;
+					break;
+			}
+			let teamNumberClimbs = 0;
+			for(let i = 0; i < 3; i++){
+				let teamEndgame = breakdown["endgameRobot" + (1 + i)];
+				if(teamEndgame === "Climbing"){
+					teamNumberClimbs++;
+				}
+			}
+			if(teamNumberClimbs === 2){
+				this.numberDoubleClimbs++;
+			} else if(teamNumberClimbs === 3){
+				this.numberTripleClimbs++;
+			}
+			let autoSwitchSeconds = breakdown.autoSwitchOwnershipSec;
+			this.teamAutoSwitchOwnershipTotalSeconds += autoSwitchSeconds;
+			if(autoSwitchSeconds > 0 || breakdown.autoSwitchAtZero) this.timesTeamOwnSwitchAfterAuto++;
+
+			let autoScaleSeconds = breakdown.autoScaleOwnershipSec;
+			this.teamAutoScaleOwnershipTotalSeconds += autoScaleSeconds;
+			if(autoScaleSeconds > 0) this.timesTeamOwnScaleAfterAuto++;
+
+			this.teamTeleopSwitchOwnershipTotalSeconds += breakdown.teleopSwitchOwnershipSec;
+			this.teamTeleopScaleOwnershipTotalSeconds += breakdown.teleopScaleOwnershipsec;
+
+			if(breakdown.vaultBoostPlayed > 0) this.timesBoostPlayed++;
+			if(breakdown.vaultForcePlayed > 0) this.timesForcePlayed++;
+			if(breakdown.vaultLevitatePlayed === 3) this.timesLevitatePlayed++;
+			this.totalCubesAtMatchEndTotal += breakdown.vaultBoostTotal + breakdown.vaultForceTotal + breakdown.vaultLevitateTotal;
+
+		}
+	}
+}
--- team/index.js
+++ team/index.js
@@ -11,24 +11,34 @@ function onInputKey(inputElement){
 			}
 		} else {
 			if(typeof value !== 'number') throw "Unexpected value: " + value;
-			
+
 			teamNumber = value;
 		}
+		if(getCurrentTeamNumber() === teamNumber){
+			return;
+		}
 		setCurrentTeamNumber(teamNumber);
+		setTagsTextToNull();
 		updateTeamData();
 	}
 }
 
 function updateTeamData(){
 	let teamNumber = getCurrentTeamNumber(); // null or number
+	if(teamNumber === null){
+		console.log("tried updating team data with no team number.");
+		setTagsTextToNull();
+		return;
+	}
 	let authKey = getAuthKey();
 	if(authKey === null){
 		let result = prompt("Please enter auth key");
 		if(result === null){
-			alert("Please enter auth key next time. (Reload the page to reenter)");
+			alert("Please enter auth key next time. (Reload the page to reenter or press enter in team number input.)");
 			return;
 		}
 		setAuthKey(result);
+		authKey = getAuthKey();
 	}
 	console.log("Updating data for : " + teamNumber);
 
@@ -44,10 +54,7 @@ function updateTeamData(){
 		setClassText("current_team_name", jsonObject.nickname);
 		let website = jsonObject.website;
 		if(website === null){
-			for (let link of document.getElementsByClassName("current_team_website_link")) {
-				link.href = "";
-				link.classList.add("disable");
-			}
+			disableWebsite();
 			setClassText("current_team_website_label", "No website");
 		} else {
 			for (let link of document.getElementsByClassName("current_team_website_link")) {
@@ -58,15 +65,126 @@ function updateTeamData(){
 		}
 		setClassText("current_team_location", jsonObject.city + ", " + jsonObject.state_prov + " (" + jsonObject.postal_code + "), " + jsonObject.country)
 	}, function(){
-		console.log("Got error when updating team data.");
-		setTagsTextToNull();
+		console.error("Got error when updating team data from team/frc");
+		// setTagsTextToNull();
 	});
+
+	getJsonData("team/frc" + teamNumber + "/matches/" + getDesiredYear(), authKey, function(jsonObject){
+		let totalArray = [0, 0, 0];
+		let qualArray = [0, 0, 0];
+		let playoffArray = [0, 0, 0];
+		let successAutoRuns = 0;
+		let unknownAutoRuns = 0;
+		console.log(jsonObject);
+		for(let match of jsonObject){
+			let isPlayoff = match.comp_level !== "qm";
+			let subArray = (isPlayoff ? playoffArray : qualArray);
+			// console.log(match.alliances.blue);
+			let isOnBlue = match.alliances.blue.team_keys.includes("frc" + teamNumber);
+			if((match.winning_alliance === "blue" && isOnBlue) || (match.winning_alliance === "red" && !isOnBlue)){
+				// we won
+				totalArray[0] += 1;
+				subArray[0] += 1;
+			} else if(match.winning_alliance === "red" || match.winning_alliance === "blue"){ // check to make sure winning_alliance is valid
+				// we lost
+				totalArray[1] += 1;
+				subArray[1] += 1;
+			} else {
+				// possibly a tie
+				if(match.score_breakdown === null){
+					//tie
+					totalArray[2] += 1;
+					subArray[2] += 1;
+				} else {
+					console.log("winning_alliance is: '" + match.winning_alliance + "'. It is either a tie or we will use the score to figure out who won.");
+					let bluePoints = match.score_breakdown.blue.total_points;
+					let redPoints = match.score_breakdown.red.total_points;
+					if (bluePoints === redPoints) {
+						//tie
+						totalArray[2] += 1;
+						subArray[2] += 1;
+					}
+					if ((bluePoints > redPoints && isOnBlue) || (redPoints > bluePoints && !isOnBlue)) {
+						// win
+						totalArray[0] += 1;
+						subArray[0] += 1;
+					} else {
+						// loss
+						totalArray[1] += 1;
+						subArray[1] += 1;
+					}
+				}
+			}
+			let robotAuto;
+			if(match.score_breakdown === null){ // 2014 and before is always like this
+				robotAuto = "Unknown";
+			} else {
+				if (isOnBlue) {
+					robotAuto = match.score_breakdown.blue["autoRobot" + (1 + match.alliances.blue.team_keys.indexOf("frc" + teamNumber))];
+				} else {
+					robotAuto = match.score_breakdown.red["autoRobot" + (1 + match.alliances.red.team_keys.indexOf("frc" + teamNumber))];
+				}
+			}
+			if(robotAuto === "Unknown"){
+				unknownAutoRuns++;
+			} else if(robotAuto !== "None"){
+				successAutoRuns++;
+			}
+		}
+		setClassText("current_record_total", totalArray[0] + "-" + totalArray[1] + "-" + totalArray[2]);
+		setClassText("current_record_qual", qualArray[0] + "-" + qualArray[1] + "-" + qualArray[2]);
+		setClassText("current_record_playoff", playoffArray[0] + "-" + playoffArray[1] + "-" + playoffArray[2]);
+		let unknownAutoRunsString = "";
+		if(unknownAutoRuns !== 0){
+			unknownAutoRunsString = " successful | and " + unknownAutoRuns + " unknown";
+		}
+		setClassText("success_auto_runs", successAutoRuns + unknownAutoRunsString);
+		setClassText("matches_played_number", jsonObject.length);
+	}, function(){
+		console.error("Was unable to get matches.");
+	});
+
+	getJsonData("team/frc" + teamNumber + "/events/" + getDesiredYear(), authKey, function(jsonObject){
+		let eventsInfo = "(" + jsonObject.length + ") ";
+		console.log(jsonObject);
+
+		let eventsArray = jsonObject;
+		eventsArray.sort(function(event, event2){
+			return +getEventDate(event) - +getEventDate(event2);
+		});
+		for(let i = 0; i < eventsArray.length; i++){
+			let event = eventsArray[i];
+			eventsInfo += event.name;
+			if(i === (eventsArray.length - 2)){
+				eventsInfo += ", and ";
+			} else if(i !== (eventsArray.length - 1)){
+				eventsInfo += ", ";
+			}
+		}
+		setClassText("events_attended", eventsInfo);
+	}, function(){
+		console.error("Got error when getting events.");
+	});
+}
+function disableWebsite(){
+	for (let link of document.getElementsByClassName("current_team_website_link")) {
+		link.href = "";
+		link.classList.add("disable");
+	}
 }
 function setTagsTextToNull(){
 	setClassText("current_team_name", null);
-	setClassText("current_team_website_link", "");
+	disableWebsite();
 	setClassText("current_team_website_label", null);
 	setClassText("current_team_location", null);
+
+	setClassText("current_record_total", null);
+	setClassText("current_record_qual", null);
+	setClassText("current_record_playoff", null);
+
+	setClassText("success_auto_runs", null);
+	setClassText("matches_played_number", null);
+	setClassText("events_attended", null);
 }
 
 (function(){ // main function
@@ -75,6 +193,12 @@ function setTagsTextToNull(){
 		setCurrentTeamNumber(null);
 	} else {
 		setCurrentTeamNumber(+teamNumber);
+	}
+	let desiredYear = getQueryObject()["year"]; // string, undefined, or "null"
+	if(desiredYear === undefined || desiredYear === "null"){
+		setDesiredYear(new Date().getFullYear());
+	} else {
+		setDesiredYear(+desiredYear);
 	}
 	setTagsTextToNull();
 	updateTeamData()
